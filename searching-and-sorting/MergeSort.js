ffef3e687c0be585151d491d6f5c835cff877006
Saran Karthik <101957704+isarankarthik@users.noreply.github.com>
2025-08-29 09:31:05 +0530

    Add files via upload
    Merge Sort -> Leetcode problem number -> 912

---
+++ searching-and-sorting/MergeSort.js
@@ -0,0 +1,28 @@
+function mergeSort(nums) {
+  if (nums.length == 1) return nums;
+
+  let mid = Math.floor(nums.length / 2);
+
+  let left = mergeSort(nums.slice(0, mid));
+
+  let right = mergeSort(nums.slice(mid));
+
+  return mergeSortHelper(left, right);
+}
+
+function mergeSortHelper(left, right) {
+  let res = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < left.length && j < right.length) {
+    if (left[i] < right[j]) {
+      res.push(left[i]);
+      i++;
+    } else {
+      res.push(right[j]);
+      j++;
+    }
+  }
+  return [...res, ...left.slice(i), ...right.slice(j)];
+}
