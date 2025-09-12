1441f47f287f175c9074d78a43937114f59d53a0
saran.karthik <saran.karthik@mydigitalaccounts.com>
2025-09-12 11:02:38 +0530

    Removing Middle Node from the Linked list -> Leet code problem number 2095

---
+++ linked-list/RemoveMiddleNodeFromLinkedList.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+function removeMiddleNode(head)
+{
+    if (head.next == null) return null;
+
+    let slow = head;
+    let fast = head.next.next;
+    while (fast && fast.next.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    slow.next = slow.next.next;
+    return head;
+}
