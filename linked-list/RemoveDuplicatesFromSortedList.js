1fc5025de26174cc0034f27a6593788affdccdb2
saran.karthik <saran.karthik@mydigitalaccounts.com>
2025-10-13 10:24:26 +0530

    Rectified the mistake in the Remove Duplicates from the Sorted List

--- linked-list/RemoveDuplicatesFromSortedList.js
+++ linked-list/RemoveDuplicatesFromSortedList.js
@@ -14,7 +14,7 @@ function removeDuplicates(head)
 {
     let curr = head;
     while (curr && curr.next) {
-        if (curr.val == curr.next.next.val) {
+        if (curr.val == curr.next.val) {
             curr.next = curr.next.next;
         } else {
             curr = curr.next;
